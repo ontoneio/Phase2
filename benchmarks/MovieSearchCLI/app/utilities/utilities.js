34fb14941ebb8fa855f5680197985e21acd8c351
Baibhav <baibhavx@gmail.com>
2017-09-04 19:18:11 -0700

    Working version

---
+++ benchmarks/MovieSearchCLI/app/utilities/utilities.js
@@ -0,0 +1,26 @@
+let http = require('http')
+let expect = require('chai').expect
+
+const sendGETRequest = (options, callback) => 
+  http.get(options, callback)
+
+const trimMovieList = movies =>
+  movies.slice(0, -2).map(movieTitle =>
+    movieTitle.indexOf('-') === -1 ? movieTitle : movieTitle.slice(0, movieTitle.indexOf('-')))
+  
+
+const printMovieList = movies =>
+  movies.forEach(movie =>
+    console.log(`${movie}\n`))
+
+const getMoviesList = ($, callback) =>
+  callback(trimMovieList($('td.result_text').map(function() {
+    return $(this).text()
+  }).get()))
+
+module.exports = {
+  sendGETRequest,
+  trimMovieList,
+  printMovieList,
+  getMoviesList
+}
