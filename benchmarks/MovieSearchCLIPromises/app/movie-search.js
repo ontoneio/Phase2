e7f0dca0b5ce863353b9f65aee733dc7ea0d2716
Baibhav <baibhavx@gmail.com>
2017-09-06 18:00:34 -0700

    Add HTTP get with callback

--- benchmarks/MovieSearchCLIPromises/app/movie-search.js
+++ benchmarks/MovieSearchCLIPromises/app/movie-search.js
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
+const http = require('http')
+let argument = process.argv.slice(2).join(' ')
 
-console.log('Hello from movie-search.js')
+let options = {
+  host: 'www.imdb.com',
+  path: `/find?ref_=nv_sr_fn&q=${process.argv[2]}&s=all`
+}
+
+let req = http.get(options, (res) => {
+  let body = ''
+  res.on('data', (chunk) => body += chunk)
+  res.on('end', () => {
+    console.log(`Received Body : ${body}`)
+  })
+
+})
+
+  req.on('error', (e) => {
+    console.log(`ERROR: ${e.message}`)
+  })
