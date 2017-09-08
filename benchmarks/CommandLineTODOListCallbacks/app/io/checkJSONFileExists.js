33f821b77fd24e6afbb5234838db0ae338287839
Baibhav <baibhavx@gmail.com>
2017-09-08 09:53:15 -0700

    Update add to take sentences with spaces

--- benchmarks/CommandLineTODOListCallbacks/app/io/checkJSONFileExists.js
+++ benchmarks/CommandLineTODOListCallbacks/app/io/checkJSONFileExists.js
@@ -4,6 +4,7 @@ const pathToTask = path.resolve(__dirname, '../tasks.json')
 
 const checkJSONFileExists = (callback) => {
   fs.stat(pathToTask, (err, stat) => callback(err, stat))
+  // fs.stat(pathToTask, callback) ?
 }
 
 module.exports = checkJSONFileExists 
