c3366a5ed92a27317f146e0ac3d968038a9f2ba4
Baibhav <baibhavx@gmail.com>
2017-09-18 09:44:22 -0700

    Add todolist SQL tests

--- benchmarks/CommandLineTODOListSQL/test/tests.js
+++ benchmarks/CommandLineTODOListSQL/test/tests.js
@@ -1,13 +1,14 @@
 const expect = require('chai').expect
+const pg = require('../database/client.js')
+
 const {
   add,
   complete,
   deleteX
 } = require('../database/commands.js')
 
-const pg = require('../database/client.js')
+//USE A 'todolist_test' DATABASE FOR TESTING PURPOSES
 pg.connectionParameters.database = 'todolist_test'
-
 
 describe('#add', function () {
 
@@ -26,15 +27,12 @@ describe('#add', function () {
     })
   })
 
-  it('Supplies the callback an error if task description is not provided', function (done) {
+  it('Supplies the callback an error if task description is absent', function (done) {
     add(undefined, function (error) {
       expect(error).to.not.equal(null)
       done()
     })
   })
-
-
-
 
 })
 
@@ -45,7 +43,6 @@ describe('#complete', function () {
       done(error)
     })
   })
-
 
   it('Completes a task from the database', function (done) {
     add('Eat Bread', function (error) {
@@ -68,6 +65,49 @@ describe('#complete', function () {
     })
   })
 
+  it('Throws an Error for invalid task Ids', function (done) {
+    complete(1, function (error) {
+      expect(error).to.not.equal(null)
+      done()
+    })
+  })
 
+})
+
+describe('#delete', function () {
+
+  beforeEach(function (done) {
+    pg.query(`truncate tasks`, (error) => {
+      done(error)
+    })
+  })
+
+  it('Deletes the task with taskId from the database', function (done) {
+    add('Eat Bread', function (error) {
+      add('Drink Milk', function (error) {
+        add('Wash Dishes', function (error) {
+          pg.query(`
+              SELECT id FROM tasks WHERE description='Drink Milk'
+            `, function (error, result) {
+            deleteX(result.rows[0].id, function (error) {
+              pg.query(`
+                  SELECT * FROM tasks WHERE description='Drink Milk'
+                `, function (error, result) {
+                expect(result.rows.length).to.equal(0)
+                done()
+              })
+            })
+          })
+        })
+      })
+    })
+  })
+
+  it('Throws an Error for invalid task Ids', function (done) {
+    deleteX(1, function (error) {
+      expect(error).to.not.equal(null)
+      done()
+    })
+  })
 
 })
